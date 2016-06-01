14651e62018374c4d9cf11047f111f1ba5d201ef
ekougs <ekougs@gmail.com>
2016-06-01 21:59:19 +0000

    Collections part 2 and end

--- src/ii_collections/_23_CompoundTasks.kt
+++ src/ii_collections/_23_CompoundTasks.kt
@@ -2,17 +2,24 @@ package ii_collections
 
 fun Shop.getCustomersWhoOrderedProduct(product: Product): Set<Customer> {
     // Return the set of customers who ordered the specified product
-    todoCollectionTask()
+    return customers.filter { customer -> customer.orderedProducts.contains(product) }.toSet()
 }
 
 fun Customer.getMostExpensiveDeliveredProduct(): Product? {
     // Return the most expensive product among all delivered products
     // (use the Order.isDelivered flag)
-    todoCollectionTask()
+    return orders.filter { order -> order.isDelivered }
+            .flatMap { order -> order.products }
+            .maxBy { product -> product.price }
+}
+
+fun Order.occurrence(product: Product): Int {
+    return products.count { currentProduct -> currentProduct == product }
 }
 
 fun Shop.getNumberOfTimesProductWasOrdered(product: Product): Int {
     // Return the number of times the given product was ordered.
     // Note: a customer may order the same product for several times.
-    todoCollectionTask()
+    return customers.flatMap { customer -> customer.orders }
+            .fold(0, {count, order -> count + order.occurrence(product)})
 }
