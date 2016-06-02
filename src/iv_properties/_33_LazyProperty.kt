56dcb94acb6c088c4402fd4a50bec57852dcb100
ekougs <ekougs@gmail.com>
2016-06-02 23:56:03 +0000

    Properties part 2

--- src/iv_properties/_33_LazyProperty.kt
+++ src/iv_properties/_33_LazyProperty.kt
@@ -1,13 +1,20 @@
 package iv_properties
 
 import util.TODO
+import java.util.concurrent.CompletableFuture
 
 class LazyProperty(val initializer: () -> Int) {
-    val lazy: Int = todoTask33()
+    val _lazy: CompletableFuture<Int> = CompletableFuture()
+    val lazy: Int get():Int {
+        if (!_lazy.isDone) {
+            _lazy.complete(initializer());
+        }
+        return _lazy.get()
+    }
 }
 
 fun todoTask33(): Nothing = TODO(
-    """
+        """
         Task 33.
         Add a custom getter to make the 'lazy' val really lazy.
         It should be initialized by the invocation of 'initializer()'
@@ -15,5 +22,5 @@ fun todoTask33(): Nothing = TODO(
         You can add as many additional properties as you need.
         Do not use delegated properties!
     """,
-    references = { LazyProperty({ 42 }).lazy }
-)
+        references = { LazyProperty({ 42 }).lazy }
+                                )
